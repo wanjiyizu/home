9f717674dc822b2034e89da294113aea698c00a0
wanjiyizu <1042320028@qq.com>
2016-12-09 17:50:34 +0800

    [web3.0]:add tow new page

--- web3.0/gulpfile.js
+++ web3.0/gulpfile.js
@@ -20,44 +20,6 @@ var gulp = require('gulp'),
     runSequence = require('run-sequence'),
     livereload = require('gulp-livereload');
 
-var paths = {
-    js: [
-        'src/js/*'
-    ],
-    css: [
-         'src/css/*'
-    ],
-    img: [
-         'src/images/*'
-    ],
-    html: [
-        'src/view/*'
-    ],
-    lib: {
-        js: [
-            'src/bower_components/jquery/dist/jquery.min.js'
-        ]
-    }
-};
-
-var output = "dist"; 
-
-gulp.task('develop', function() {
-    gulp.src(paths.js)
-        .pipe(gulp.dest(output + '/js'));
-        
-    gulp.src(paths.lib.js)
-        .pipe(gulp.dest(output + '/js'));
-
-    gulp.src(paths.css)
-        .pipe(gulp.dest(output + '/css'));
-        
-    gulp.src(paths.img)
-        .pipe(gulp.dest(output + '/images'));
-
-    gulp.src(paths.html)
-        .pipe(gulp.dest(output + "/view"));
-});
 
 gulp.task("default", ['clean'], function(){
 	//gulp.start( 'reference', 'js', 'connect', 'watch');
@@ -70,31 +32,15 @@ gulp.task('clean', function() {
 });
 
 gulp.task('styles', function() {  
-  return gulp.src(['src/css/*.css'])
-    .pipe(plumber())
-	.pipe(changed("dist/css/"))
-    .pipe(cssimport())
-	.pipe(autoprefixer({browsers: ['last 2 versions'],
+  return gulp.src(['src/css/**/*.css'])
+        .pipe(plumber())
+        .pipe(cssimport())
+	     .pipe(autoprefixer({browsers: ['last 2 versions'],
             cascade: false}))
-  	.pipe(minifycss())
+  	//.pipe(minifycss())
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('reference', ['htmlput', 'import'], function(){
-    //gulp.start('usemin', "imgput");
-});
-
-gulp.task('reloadhtml', ['htmlput', 'import'], function(){
-    gulp.start('usemin');
-});
-
-gulp.task('reloadcss', ['import'], function(){
-    return gulp.src(['temp/css/*.css'])
-                .pipe(plumber())
-                .pipe(minifycss())
-                .pipe(rename({ suffix: '.min' }))
-                .pipe(gulp.dest('dist/css'));
-});
 
 gulp.task('htmlput', function() {
     return gulp.src(["src/**/*.html"])
@@ -103,59 +49,16 @@ gulp.task('htmlput', function() {
 				.pipe(gulp.dest("dist"));
 });
 
-gulp.task("import", function() {
-    return gulp.src("src/css/*.css")
-        .pipe(cssimport())
-        .pipe(gulp.dest("temp/css/"));
-}); 
-
 
 gulp.task("imgput", function(){
     gulp.src("src/images/**/*.*")
         .pipe(gulp.dest("dist/images/"))
 });
 
-gulp.task('usemin', function() {
-    var options = {
-            removeComments: true,//清除HTML注释
-            collapseWhitespace: true,//压缩HTML
-            collapseBooleanAttributes: false,//省略布尔属性的值 <input checked="true"/> ==> <input />
-            removeEmptyAttributes: false,//删除所有空格作属性值 <input id="" /> ==> <input />
-            removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
-            removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
-            minifyJS: true,//压缩页面JS
-            minifyCSS: true//压缩页面CSS
-        },
-        useminOpts = {
-            css: [ rev() ]
-        }
-
-    gulp.src(["dist/*.html", "dist/common/*.html"])
-        .pipe(usemin({
-            css: [minifycss()]
-        }))
-        .pipe(gulp.dest("dist/"));
-});
-
-
 gulp.task('js', function(){
     return gulp.src("src/js/**/*.js")
            .pipe(plumber())
            .pipe(gulp.dest("dist/js"));
-});
-
-gulp.task('rev', function() {
-    gulp.src(['./rev/*.json', './src/view/*.html'])
-        .pipe(plumber())
-        .pipe(revCollector())
-        .pipe(gulp.dest('./dist/view/'));
-});
-
-gulp.task("jsmin", function(){
-	gulp.src(['src/js/dialog.js', 'src/js/base.js'])
-		.pipe(concat('common.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/js'));
 });
 
 
@@ -179,6 +82,7 @@ gulp.task('watch', function () {
 		console.log(path);
 		gulp.src("dist/"+path, {read: false}).pipe(clean());
 	}else if(e.type == "changed"){
+    console.log("=======");
 		gulp.start("styles", "refresh");
 	}else{
 		gulp.start("styles");
