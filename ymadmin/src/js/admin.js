ec3437473eb6f66e11a4d5645078ac2a1d99a3dc
wanjiyizu <1042320028@qq.com>
2016-12-16 17:58:36 +0800

    [ymadmin]:adjust the page style

--- ymadmin/src/js/admin.js
+++ ymadmin/src/js/admin.js
@@ -1,15 +1,23 @@
 (function(win, document){
-	$(".sidebar-toggle i").on("click", function(){
+	$(".sidebar-closed").on("click", function(){
 		if($(this).data("toggle")){
 			$(this).data("toggle", false);
-			$(".page-sidebar").hide();
-			$(".page-content").css("margin-left", 0);
+			$(".offcanvas-bar").removeClass("open").addClass("closed")
+			setTimeout(function(){
+				$(".page-sidebar").hide();
+			}, 200);
 		}else{
 			$(this).data("toggle", true);
-			$(".page-sidebar").show();
-			$(".page-content").css("margin-left", 235);
+			$(".page-sidebar").show();	
+			setTimeout(function(){
+				$(".offcanvas-bar").removeClass("closed").addClass("open");
+			},100);
 		}
 		
+	});
+
+	$(window).on("resize", function(){
+		$(".offcanvas-bar").removeClass("open");
 	});
 
 	$(".sidebar-dropdown").on("click", function(){
@@ -19,7 +27,7 @@
 
 	});
 
-	$(".admin-menu").on("click", function(){
+	$("#admin-menu").on("click", function(){
 		$(".page-sidebar").addClass("offcanvas-active");
 		setTimeout(function(){
 			$(".page-sidebar .offcanvas-bar").addClass("offcanvas-bar-active");
@@ -31,7 +39,52 @@
 		$(".page-sidebar .offcanvas-bar").removeClass("offcanvas-bar-active");
 		setTimeout(function(){
 			$(".page-sidebar").removeClass("offcanvas-active");
-		},200);
+		},300);
+	});
+
+	$(".offcanvas-bar").on("click", function(e){
+		e.stopImmediatePropagation();
+	});
+
+
+	var startX, startY, $this = $("body"), _left, _top, left, top, active = true;
+	// $(document).on("touchstart", function(e){
+	// 	e.preventDefault();
+	// 	active = true;
+	// 	startX = e.originalEvent.targetTouches[0].pageX;
+	// 	startY = e.originalEvent.targetTouches[0].pageY;
+	// 	left = $("#admin-menu").offset().left;
+	// 	top = $("#admin-menu").offset().top;
+	// 	// console.log(left);
+
+	// });
+
+	// $(document).on("touchmove", function(e){
+	// 	e.preventDefault();
+	// 	if(!active){
+	// 		return;
+	// 	}
+	// 	var windowW = document.documentElement.clientWidth,
+	// 		windowH = document.documentElement.clientHeight,
+	// 		offsetX = e.originalEvent.targetTouches[0].pageX,
+	// 		offsetY = e.originalEvent.targetTouches[0].pageY;
+	// 		console.log(left);
+	// 	if(offsetX <= 0 || offsetX >= windowW || left <= 0 || left >= windowW){
+	// 		console.log("超过了");
+	// 		return;
+	// 	}
+
+	// 	if(offsetY <= 0 || offsetY >= windowH || top <= 0 || top >= windowH){
+	// 		return;
+	// 	}
+
+	// 	_left = Math.abs(offsetX + left - startX);
+	// 	_top = Math.abs(offsetY + top - startY);
 		
-	})
+	// 	$("#admin-menu").css({"left": _left, "top": _top});
+	// });
+
+	// $(document).on("touchend", function(e){
+	// 	active = false;
+	// });
 })(this, document);
