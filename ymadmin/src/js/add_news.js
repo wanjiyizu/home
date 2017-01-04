f2b44c0834a0698e0479ec44b73e2d747f6dc443
wanjiyizu <1042320028@qq.com>
2017-01-04 18:04:31 +0800

    [ymadmin]:add code logic

--- ymadmin/src/js/add_news.js
+++ ymadmin/src/js/add_news.js
@@ -1,4 +1,4 @@
-(function(){
+(function(window, document){
 	var ue = UE.getEditor('container',{
 		initialFrameHeight: 540,
 	 	autoHeightEnabled: false,
@@ -49,10 +49,6 @@
 	var uploaderOpts = {
 	    auto: true,
 	    server: 'http://localhost:8080/img/test_upload.html',
-	    formData: {
-	    	"oldUrl": "http://img.youmifinance.com/img/images/news/1481098521222/cover/1481100563911.png",
-	    	"dir": "images/news/2016/cover"
-	    },
 	    accept: {
 	        title: 'Images',
 	        extensions: 'gif,jpg,jpeg,bmp,png',
@@ -61,7 +57,10 @@
 	};
 
 	var coverUploader = null,
-		thumbUploader = null;
+		thumbUploader = null,
+		realPath = null,
+		time = getTime();
+		imageFolder = "images/news/" + time;
 
 	coverUploader = WebUploader.create(uploaderOpts),
 	thumbUploader = WebUploader.create(uploaderOpts);
@@ -69,14 +68,13 @@
 	uploadHandler.call(thumbUploader, "#imgpickthumb", $("#logo-image-thumb"), 216, 99);
 
 	function uploadHandler(pickEl, $wrap, width, height){
-
-		this.addButton({
+		var uploader = this;
+		uploader.addButton({
 			id: pickEl,
 		    multiple: false
 		});
 
-		this.on("fileQueued", function(file){
-
+		uploader.on("fileQueued", function(file){
 			var $img,
 				wrap = $wrap;
 			wrap.html("<img>");
@@ -91,25 +89,59 @@
 		        $img.attr( 'src', src );
 		    },width,height);
 		});
+
+		uploader.on("uploadStart", function(file){
+			var oldUrl = $wrap.data("src"),
+				type = $wrap.data("type"),
+				dir = type == "cover" ? "images/news/"+time+"/cover" : "images/news/"+time+"/logo";
+			console.log(oldUrl + "===" + dir);
+			uploader.option("formData", {
+				"oldUrl": oldUrl,
+	    		"dir": dir
+			});
+		});
+
+		uploader.on("uploadProgress", function(file, percentage){
+			var  $percentage = $wrap.find(".progress .progress-bar");
+			if( !$percentage.length ){
+				$percentage = $wrap.append("<div class='progress'><div class='progress-bar progress-bar-warning progress-bar-striped' role='progressbar' aria-valuemin='0' aria-valuemax='100'></div>").find(".progress .progress-bar");
+			}
+			$percentage.css("width", percentage * 100 + '%');
+		});
+
+		uploader.on("uploadComplete", function(file){
+			$wrap.find(".progress").remove();
+		});
+
+		uploader.on("uploadAccept", function(file, ret){
+			$wrap.data("src", ret.src);
+		});
 	}
 
 	
 	$("#save").on("click", save);
 	$("#preview").on("click", preview);
+	$("#publish").on("click", publish);
 	
 	function save(){
 		// 获取数据
 		var news = getNewsData();
 		news["status"] = 0;
 		// 验证数据
-		verifyData(news);
+		validate(news);
 		// 提交数据
 	}
 
 	function preview(){
 		var news = getNewsData();
-		news["status"] = 2;
-		verifyData(news);
+		news["status"] = 0;
+		validateStrictly(news);
+	}
+
+	function publish(){
+		var news = getNewsData();
+		news["status"] = 1;
+		validateStrictly(news);
 	}
 
 	function getNewsData(){
@@ -122,31 +154,65 @@
 			uploader: $("#js-news-author").val(),
 			source: $("#js-news-source").val(),
 			date: $("#js-news-date").val(),
-			status: $("#save").data("status") || 0
+			status: $("#save").data("status") || 0,
+			realPath: realPath || "",
+			imgDir: imageFolder
 		}
 		return data;
 	}
 
-	function verifyData(obj){
-		if(obj == null){
-			return false;
+	function validate(data){
+		if(util.isEmpty(obj.title) && util.isEmpty(obj.content)){
+			$("#page-tip").text("请先输入一段正文（或者标题），再点击保存按钮。").parent().show();
+			console.log("请先输入一段正文（或者标题），再点击保存按钮。");
 		}
-
-		/*预览和保存并发布的情况下*/
-		if(obj.status == 1 || obj.status == 2){
-			if(util.isEmpty(obj.title)){
-				console.log("标题不能为空");
-			}else if(util.isEmpty(obj.content)){
-				console.log("正文必须有文字，请在正文中至少输入1个汉字后重试");
-			}
-		}else{
-			/*保存的情况下*/
-			if(util.isEmpty(obj.title) && util.isEmpty(obj.content)){
-				console.log("请先输入一段正文（或者标题），再点击保存按钮。");
-			}
-		}
-
-		
 	}
 
-})();
+	function validateStrictly(obj){
+
+		if(util.isEmpty(obj.title)){
+			$("#page-tip").text("标题不能为空").parent().fadeIn().fadeOut(3000);
+			$("#js-news-title").focus();
+		}else if(util.isEmpty(obj.content)){
+			$("#page-tip").text("正文必须有文字，请在正文中至少输入1个汉字后重试").parent().fadeIn().fadeOut(3000);
+			ue.focus();
+		}else if(util.isEmpty(obj.brief)){
+			$("#page-tip").text("摘要不能为空").parent().fadeIn().fadeOut(3000);
+			$("#js-news-summary").focus();
+		}else if(util.isEmpty(obj.coverImg)){
+			$("#page-tip").text("必须选择一张文章封面图").parent().fadeIn().fadeOut(3000);
+		}else if(util.isEmpty(obj.logoImg)){
+			$("#page-tip").text("必须选择一张文章缩略图").parent().fadeIn().fadeOut(3000);
+		}else if(util.isEmpty(obj.uploader)){
+			$("#page-tip").text("发布者不能为空").parent().fadeIn().fadeOut(3000);
+			$("#js-news-author").focus();
+		}else if(util.isEmpty(obj.source)){
+			$("#page-tip").text("文章来源不能为空").parent().fadeIn().fadeOut(3000);
+			$("#js-news-source").focus();
+		}else if(util.isEmpty(util.isEmpty(obj.date))){
+			$("#page-tip").text("时间不能为空").parent().fadeIn().fadeOut(3000);
+			$("#js-news-date").focus();
+		}
+	}
+
+	function getTime(){
+		var time = $("#save").data("time"),
+			stroageTime = localStorage.getItem("time");
+		if(util.isNotEmpty(stroageTime)){
+			time = stroageTime;
+		}else if(util.isEmpty(time) && util.isEmpty(stroageTime)){
+			time = new Date().getTime();
+		}
+
+		return time;
+	}
+
+	window.onbeforeunload = function(){
+		// return "为确保内容不丢失";
+		var data = getNewsData();
+		var time = new Date().getTime();
+		localStorage.setItem(time, data);
+		return true;
+	}
+
+})(this, document);
