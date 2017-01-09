54a82beb3830fd1771eecc43b4ef4815b369c943
wanjiyizu <1042320028@qq.com>
2017-01-09 17:56:57 +0800

    [ymadmin]:add code logic

--- ymadmin/src/js/add_news.js
+++ ymadmin/src/js/add_news.js
@@ -1,50 +1,75 @@
 (function(window, document){
+
+	// $('#js-news-date').datetimepicker('update');
+	
 	var ue = UE.getEditor('container',{
-		initialFrameHeight: 540,
-	 	autoHeightEnabled: false,
-	 	serverUrl: "http://localhost:8080/img/controller.jsp",
-	 	elementPathEnabled: false,
-	 	wordCount: false,
-		toolbars: [
-		     [
-		        'undo', //撤销
-		        'redo', //重做
-		        'bold', //加粗
-		        'italic', //斜体
-		        'underline', //下划线
-		        'forecolor', //字体颜色
-		        'horizontal', //分隔线
-		        'blockquote', //引用
-		        'strikethrough', //删除线
-		        'formatmatch', //格式刷
-		        'backcolor', //背景色
-		        'link', //超链接
-		        'removeformat', //清除格式
-		        'fontfamily', //字体
-		        'fontsize', //字号
-		        'lineheight', //行间距
-		        'justifyleft', //居左对齐
-		        'justifyright', //居右对齐
-		        'justifycenter', //居中对齐
-		        'justifyjustify', //两端对齐
-		        'insertorderedlist', //有序列表
-		        'insertunorderedlist', //无序列表
-		        'rowspacingtop', //段前距
-		        'rowspacingbottom', //段后距
-		        'insertimage', //多图上传
-		        'imagenone', //默认
-		        'imageleft', //左浮动
-		        'imageright', //右浮动
-		        'imagecenter', //居中
-		        'searchreplace', //查询替换
-		        'insertvideo', //视频
-		        'autotypeset', //自动排版
-		        'music', //音乐
-		        'drafts', // 从草稿箱加载
-		        'source', //源代码
-		    ]
-		]
-	});
+			initialFrameHeight: 540,
+		 	autoHeightEnabled: false,
+		 	serverUrl: "http://localhost:8080/img/controller.jsp",
+		 	elementPathEnabled: false,
+		 	wordCount: false,
+			toolbars: [
+			     [
+			        'undo', //撤销
+			        'redo', //重做
+			        'bold', //加粗
+			        'italic', //斜体
+			        'underline', //下划线
+			        'forecolor', //字体颜色
+			        'horizontal', //分隔线
+			        'blockquote', //引用
+			        'strikethrough', //删除线
+			        'formatmatch', //格式刷
+			        'backcolor', //背景色
+			        'link', //超链接
+			        'removeformat', //清除格式
+			        'fontfamily', //字体
+			        'fontsize', //字号
+			        'lineheight', //行间距
+			        'justifyleft', //居左对齐
+			        'justifyright', //居右对齐
+			        'justifycenter', //居中对齐
+			        'justifyjustify', //两端对齐
+			        'insertorderedlist', //有序列表
+			        'insertunorderedlist', //无序列表
+			        'rowspacingtop', //段前距
+			        'rowspacingbottom', //段后距
+			        'insertimage', //多图上传
+			        'imagenone', //默认
+			        'imageleft', //左浮动
+			        'imageright', //右浮动
+			        'imagecenter', //居中
+			        'searchreplace', //查询替换
+			        'insertvideo', //视频
+			        'autotypeset', //自动排版
+			        'music', //音乐
+			        'drafts', // 从草稿箱加载
+			        'source', //源代码
+			    ]
+			]
+		});
+
+	// initEditor();
+
+	function initEditor(){
+		var news = localStorage.getItem("news"),
+			obj = JSON.parse(news);
+		if(obj == null){
+			return false;
+		}
+		ue.ready(function(){
+			ue.setContent(obj.content);
+		});
+
+		$("#js-news-title").val(obj.title);
+		$("#js-news-summary").val(obj.brief);
+		$("#cover-image-thumb").data("src", obj.coverImg);
+		$("#logo-image-thumb").data("src", obj.logoImg);
+		$("#js-news-author").val(obj.uploader);
+		$("#js-news-source").val(obj.source);
+		$("#js-news-date").val(obj.date);
+		
+	}
 
 	var uploaderOpts = {
 	    auto: true,
@@ -69,6 +94,7 @@
 
 	function uploadHandler(pickEl, $wrap, width, height){
 		var uploader = this;
+
 		uploader.addButton({
 			id: pickEl,
 		    multiple: false
@@ -158,8 +184,11 @@
 			realPath: realPath || "",
 			imgDir: imageFolder
 		}
+		console.log(JSON.stringify(data));
 		return data;
 	}
+
+
 
 	function validate(data){
 		if(util.isEmpty(obj.title) && util.isEmpty(obj.content)){
@@ -207,12 +236,24 @@
 		return time;
 	}
 
-	window.onbeforeunload = function(){
-		// return "为确保内容不丢失";
-		var data = getNewsData();
-		var time = new Date().getTime();
-		localStorage.setItem(time, data);
-		return true;
-	}
+	// window.onbeforeunload = function(){
+	// 	var data = getNewsData();
+	// 	// localStorage.setItem("news", JSON.stringify(data));
+	// 	localStorage.setItem("news", data);
+	// 	// return  "--------------------------------------------\n为确保内容不丢失\n建议点击页面底部的绿色保存按钮后再离开\n--------------------------------------------"	;
+	// 	return false;
+	// }
+
+	// window.onbeforeunload = function(){
+	// 	var data = getNewsData();
+	// 	localStorage.setItem("news", data);
+	// 	return "=====";
+	// }
+
+	// window.onbeforeunload = function(){
+	// 	var data = getNewsData();
+	// 	localStorage.setItem("news", JSON.stringify(data));
+	// 	return "--------------------------------------------\n为确保内容不丢失\n建议点击页面底部的绿色保存按钮后再离开\n--------------------------------------------";
+	// }
 
 })(this, document);
